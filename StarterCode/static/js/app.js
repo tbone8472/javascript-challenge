a71c074075ecb6bc6b2870d7868bd5d1f633e16b
TBone8472 <Mavericks@Bellerophon.fios-router.home>
2020-05-09 20:09:58 -0400

    stage 2

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,23 @@
+
 // from data.js
 var tableData = data;
+console.log(tableData)
 
-// YOUR CODE HERE!
+//Selecting the button
+var button = d3.select("#filter-btn");
+console.log(button)
+
+button.on("click", function() {
+    
+//Select the input element; fetch raw input
+var input = d3.select("#datetime")
+console.log(input)
+
+//checking if the value resident in the variable
+var inputValue = input.property("value");
+console.log(inputValue)
+
+dTime = tableData.filter(function(data){ return (inputValue === data.datetime)})
+console.log(dTime)
+
+});
